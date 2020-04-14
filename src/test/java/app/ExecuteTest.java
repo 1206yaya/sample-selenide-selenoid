84e255941ed3f56bd58d2e9d36b9f00dfa790d6e
1206yaya <1206yaya@gmail.com>
2020-04-14 14:05:04 +0900

    first commit

---
+++ src/test/java/app/ExecuteTest.java
@@ -0,0 +1,12 @@
+package app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class ExecuteTest {
+	@Test
+	public void execTest() {
+		assertTrue(true);
+	}
+}
